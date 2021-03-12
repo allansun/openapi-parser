a5845070e4966252e5a193680d75b2d458123cc7
Allan Sun <allan.sun@bricre.com>
2021-03-12 16:33:17 +0000

    Update OpenAPIParserTest to follow latest demo json

--- tests/OpenAPIParserTest.php
+++ tests/OpenAPIParserTest.php
@@ -3,7 +3,7 @@
 namespace Tests\OpenAPI;
 
 use OpenAPI\OpenAPIParser;
-use OpenAPI\Schema\V3\OpenAPI;
+use OpenAPI\Schema\V3 as Schema;
 use PHPUnit\Framework\TestCase;
 
 class OpenAPIParserTest extends TestCase
@@ -18,8 +18,36 @@ class OpenAPIParserTest extends TestCase
 
         $result = $parser->parse($data);
 
-        $this->assertInstanceOf(OpenAPI::class, $result);
+        $this->assertInstanceOf(Schema\OpenAPI::class, $result);
 
         $this->assertMatchesRegularExpression('/(.*)Petstore(.*)/', $result->info->title);
+
+        $this->assertInstanceOf(Schema\OpenAPI::class, $result);
+
+        $Info = $result->info;
+
+        $this->assertInstanceOf(Schema\Info::class, $Info);
+
+        $Paths = $result->paths;
+
+        $this->assertInstanceOf(Schema\Paths::class, $Paths);
+
+        /** @var Schema\PathItem $PetsPath */
+        $PetsPath = $Paths->getPatternedField('/pet');
+
+        $this->assertInstanceOf(Schema\PathItem::class, $PetsPath);
+
+        $GetOperation = $PetsPath->put;
+
+        $this->assertInstanceOf(Schema\Operation::class, $GetOperation);
+
+        $responses = $GetOperation->responses;
+
+        $this->assertInstanceOf(Schema\Responses::class, $responses);
+
+        /** @var Schema\Response $response200 */
+        $response400 = $responses->getPatternedField('400');
+
+        $this->assertInstanceOf(Schema\Response::class, $response400);
     }
 }
