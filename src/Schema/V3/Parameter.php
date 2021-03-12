9cf67f8a1cc20b3db5cbc9076fc8c6aeda30974f
Allan Sun <allan.sun@bricre.com>
2021-03-12 16:17:29 +0000

    Update to follow specification V3.1.0

--- src/Schema/V3/Parameter.php
+++ src/Schema/V3/Parameter.php
@@ -71,14 +71,53 @@ class Parameter extends AbstractObject
     // and style can describe the structure and syntax of the parameter.
 
     /**
-     * @var ParameterSchema|null
+     * @var string|null
      */
-    public ?ParameterSchema $schema = null;
+    public ?string $style = null;
 
     /**
-     * @var ParameterStyle|null
+     * When this is true, parameter values of type array or object generate separate parameters for each value of the
+     * array or key-value pair of the map. For other types of parameters this property has no effect. When style is
+     * form, the default value is true. For all other styles, the default value is false.
+     *
+     * @var bool|null
      */
-    public ?ParameterStyle $style = null;
+    public ?bool $explode = false;
+
+    /**
+     * Determines whether the parameter value SHOULD allow reserved characters, as defined by RFC3986
+     * :/?#[]@!$&'()*+,;= to be included without percent-encoding. This property only applies to parameters with an in
+     * value of query. The default value is false.
+     *
+     * @var bool|null
+     */
+    public ?bool $allowReserved = false;
+
+    /**
+     * @var Schema|null
+     */
+    public ?Schema $schema = null;
+
+    /**
+     * Example of the parameter's potential value. The example SHOULD match the specified schema and encoding
+     * properties if present. The example field is mutually exclusive of the examples field. Furthermore, if
+     * referencing a schema that contains an example, the example value SHALL override the example provided by the
+     * schema. To represent examples of media types that cannot naturally be represented in JSON or YAML, a string
+     * value can contain the example with escaping where necessary.
+     *
+     * @var mixed
+     */
+    public $example = null;
+
+    /**
+     * Examples of the parameter's potential value. Each example SHOULD contain a value in the correct format as
+     * specified in the parameter encoding. The examples field is mutually exclusive of the example field. Furthermore,
+     * if referencing a schema that contains an example, the examples value SHALL override the example provided by the
+     * schema.
+     *
+     * @var Example[]|Reference[]|null
+     */
+    public ?array $examples = null;
 
     // For more complex scenarios, the content property can define the media type and schema of the parameter.
     // A parameter MUST contain either a schema property, or a content property, but not both. When example or examples
